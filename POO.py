974fcec41adb5fb7f4fcb9a27f2ee808cbf67870
Alexander Dueñas <alxdr.dns@outlook.com>
2022-12-05 23:31:54 -0500

    Actualización 05/12/2022

--- POO.py
+++ POO.py
@@ -7,8 +7,8 @@ class Persona:
     def hola(self):
         print('Hola, mi nombre es {} y tengo {} años'.format(self.nombre, self.edad))
 
+
 if __name__ == '__main__':
     persona = Persona('Alexander', 32)
-
-print ('Edad: {}'.format(persona.edad))
-persona.hola()
+    print ('Edad: {}'.format(persona.edad))
+    persona.hola()
